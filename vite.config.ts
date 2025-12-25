ccb15f13a1ae583f424ea1162688d0b353abe858
lulukitchenil-maker <lulu.kitchen.il@gmail.com>
2025-12-25 02:32:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-=======
   build: {
     rollupOptions: {
       output: {
@@ -14,5 +12,4 @@ export default defineConfig({
     }
   },
   publicDir: 'public'
->>>>>>> f0a58e6 (Initial commit)
 });
